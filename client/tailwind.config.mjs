9bb879ed638084fe03db3c174d83235d81ca0623
Kacper Lebiedzinski <kltpl@duck.com>
2024-01-26 13:24:51 +0100

    Added tailwindcss library to the project

---
+++ client/tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+export default {
+  content: ["./src/**/*.{html,js,astro,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ["Poppins", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
+}
+
+// background-color: hsl(216, 7%, 14%);
+// color: hsl(0, 0%, 0%);
+
