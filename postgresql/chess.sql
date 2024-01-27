b33cd415a15cdd3090464267f12e42c75f93a85a
Kacper Lebiedzinski <kltpl@duck.com>
2024-01-27 12:00:35 +0100

    Working on creating a backend for the app. Created a sql file to write code for the postgresql database. Inserted some data into the database.

---
+++ postgresql/chess.sql
@@ -0,0 +1,109 @@
+/*
+CREATE DATABASE chess;
+
+CREATE TABLE dict_game_result (
+  id SMALLSERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+INSERT INTO dict_game_result VALUES (0, 'remis');
+INSERT INTO dict_game_result VALUES (1, 'wygrana białych');
+INSERT INTO dict_game_result VALUES (2, 'wygrana czarnych');
+
+CREATE TABLE dict_game_end_reason (
+  id SMALLSERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+INSERT INTO dict_game_end_reason VALUES (1, 'mat');
+INSERT INTO dict_game_end_reason VALUES (2, 'poddanie');
+INSERT INTO dict_game_end_reason VALUES (3, 'przekroczenie limitu czasowego');
+INSERT INTO dict_game_end_reason VALUES (10, 'pat');
+INSERT INTO dict_game_end_reason VALUES (11, 'niewystarczający materiał');
+INSERT INTO dict_game_end_reason VALUES (12, 'zasada 50 ruchów');
+INSERT INTO dict_game_end_reason VALUES (13, '3 ruchy powtórzenia');
+INSERT INTO dict_game_end_reason VALUES (14, 'remis za porozumieniem stron');
+
+CREATE TABLE dict_piece (
+  id SMALLSERIAL PRIMARY KEY,
+  symbol_FEN VARCHAR(1),
+  name VARCHAR(10)
+);
+
+INSERT INTO dict_piece VALUES (1, 'p', 'pion');
+INSERT INTO dict_piece VALUES (2, 'r', 'wieża');
+INSERT INTO dict_piece VALUES (3, 'n', 'skoczek');
+INSERT INTO dict_piece VALUES (4, 'b', 'goniec');
+INSERT INTO dict_piece VALUES (5, 'q', 'hetman');
+INSERT INTO dict_piece VALUES (6, 'k', 'król');
+
+CREATE TABLE app_user (
+  id BIGSERIAL PRIMARY KEY,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  name VARCHAR(50) NOT NULL UNIQUE,
+  display_name VARCHAR(50) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  date_create TIMESTAMP NOT NULL DEFAULT NOW(),
+  is_active BOOLEAN NOT NULL DEFAULT true,
+  date_last_login TIMESTAMP
+);
+
+CREATE TABLE game (
+  id BIGSERIAL PRIMARY KEY,
+  user_id_w BIGINT NOT NULL REFERENCES app_user(id),
+  user_id_b BIGINT NOT NULL REFERENCES app_user(id),
+  result_id BIGINT REFERENCES dict_game_result(id),
+  end_reason_id BIGINT REFERENCES dict_game_end_reason(id),
+  is_finished BOOLEAN NOT NULL DEFAULT false,
+  start_date TIMESTAMP NOT NULL DEFAULT NOW(),
+  end_date TIMESTAMP,
+  castling_w_k BOOLEAN NOT NULL DEFAULT true,
+  castling_w_q BOOLEAN NOT NULL DEFAULT true,
+  castling_b_k BOOLEAN NOT NULL DEFAULT true,
+  castling_b_q BOOLEAN NOT NULL DEFAULT true
+);
+
+CREATE TABLE game_halfmove (
+  id BIGSERIAL PRIMARY KEY,
+  game_id BIGINT NOT NULL REFERENCES game(id),
+  piece_id BIGINT NOT NULL REFERENCES dict_piece(id),
+  is_user_w BOOLEAN NOT NULL,
+  halfmove_number SMALLINT NOT NULL,
+  pos_start_x SMALLINT NOT NULL,
+  pos_start_y SMALLINT NOT NULL,
+  pos_end_x SMALLINT NOT NULL,
+  pos_end_y SMALLINT NOT NULL,
+  king_checked_pos_x SMALLINT,
+  king_checked_pos_y SMALLINT,
+  is_castling BOOLEAN NOT NULL
+);
+
+INSERT INTO app_user
+(email, name, display_name, password)
+VALUES ('kltpl@email.com', 'kltpl', 'kltpl', '1234');
+
+INSERT INTO app_user
+(email, name, display_name, password)
+VALUES ('kltpl@email.com', 'klt', 'kltpl', '1234');
+
+INSERT INTO app_user
+(email, name, display_name, password)
+VALUES ('klt@email.com', 'kltpl', 'kltpl', '1234');
+
+INSERT INTO app_user
+(email, name, display_name, password)
+VALUES ('klt@email.com', 'klt', 'kltpl', '1234');
+
+INSERT INTO game 
+       (user_id_w, user_id_b)
+VALUES (1, 8);
+
+
+INSERT INTO game_halfmove
+       (game_id, piece_id, is_user_w, halfmove_number, pos_start_x, pos_start_y, pos_end_x, pos_end_y, king_checked_pos_x, king_checked_pos_y, is_castling)
+VALUES (1, 1, true, 1, 4, 6, 4, 4, NULL, NULL, false);
+*/
+
+SELECT * FROM app_user;
+SELECT * FROM game;
+SELECT * FROM game_halfmove;
