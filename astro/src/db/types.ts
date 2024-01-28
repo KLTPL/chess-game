10d06e6ff4463f97c9a76b30912f9a605811f65f
Kacper Lebiedziński <k.lebiedzinski04.gmail.com>
2024-01-28 16:13:42 +0100

    Now the /online-game/[display_id] route works (the data about the game is sent to the client) but the chess board doesn't now how to use it yet. All types related to the db are store in the /ser/db/types.ts file. Files that initialize the game on the client side have better names and are int the scripts folder

---
+++ astro/src/db/types.ts
@@ -0,0 +1,29 @@
+type DBGame = {
+  is_finished: boolean;
+  result_name: null | string;
+  end_reason_name: null | string;
+  castling_w_k: boolean;
+  castling_w_q: boolean;
+  castling_b_k: boolean;
+  castling_b_q: boolean;
+  user_w_display_name: string;
+  user_w_name: string;
+  user_b_display_name: string;
+  user_b_name: string;
+};
+type DBHalfmove = {
+  piece_symbol_fen: string;
+  halfmove_number: number;
+  pos_start_x: number;
+  pos_start_y: number;
+  pos_end_x: number;
+  pos_end_y: number;
+  king_checked_pos_x: null | number;
+  king_checked_pos_y: null | number;
+  is_castling: boolean;
+};
+
+export type DBGameData = {
+  game: DBGame;
+  halfmoves: DBHalfmove[];
+};
