59557e3975239e29795f2081377fc84c2e7e366b
Kacper Lebiedziński <k.lebiedzinski04.gmail.com>
2024-02-16 19:46:21 +0100

    Created a protect routes system. Now all protected routs should be defined in /src/middleware/protect. Also moved the jwt sign and verify functions to a different folder.

---
+++ astro/src/middleware/protect.ts
@@ -0,0 +1,45 @@
+import { defineMiddleware } from "astro:middleware";
+import verify from "../utils/jwt/verify";
+
+const PROTECTED_PATHS = ["/online", "/online-game/*"];
+
+const protect = defineMiddleware(({ url, cookies, redirect }, next) => {
+  try {
+    const isProtected = isPathProtected(url.pathname);
+
+    if (isProtected) {
+      const token = cookies.get("token")?.value;
+      if (token === undefined) {
+        console.log("no token so redirect");
+        return redirect("/login");
+      }
+  
+      const isVerified = verify(token);
+      
+      if (!isVerified) {
+        cookies.delete("token", { path: "/" });
+        return redirect("/login");
+      }
+    }
+
+    return next();
+  } catch (err) {
+    console.error(err);
+    return next();
+  }
+});
+
+function isPathProtected(pathname: string) {
+  for (const path of PROTECTED_PATHS) {
+    const idx = pathname.indexOf(path.slice(0, -1));
+    if (
+      path === pathname ||
+      (path.slice(path.length - 2, path.length) === "/*" && idx === 0)
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export default protect;
