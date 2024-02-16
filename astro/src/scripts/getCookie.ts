3026736bfa8caf3fcc08b3055a4630219e9003a5
Kacper Lebiedzinski <kltpl@duck.com>
2024-02-16 23:34:18 +0100

    Added login, register and logout buttons to the navbar. Installed a library js-cookie for handling cookies.

--- astro/src/scripts/getCookie.ts
+++
@@ -1,8 +0,0 @@
-export default function getCookie(name: string): string {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) {
-    return (parts.pop() as string).split(";").shift() as string;
-  }
-  return "";
-}
