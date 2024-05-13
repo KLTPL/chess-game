0981666872c0bdd2be06e23a10291e5aa14a968a
Kacper Lebiedzinski <kltpl@duck.com>
2024-05-13 17:40:18 +0200

    Fixed a visual bug

--- src/components/friends/search-users/SearchUsers.tsx
+++ src/components/friends/search-users/SearchUsers.tsx
@@ -117,23 +117,25 @@ export default function SearchUsers({
       <div className="my-3 flex w-[97vw] grow flex-col items-stretch self-center overflow-auto md:w-[70ch]">
         {searchDataStatus === "loading" && (
           <div
-            className="mt-3 flex grow animate-spin items-center justify-center text-lg text-white"
+            className="mt-3 flex grow items-center justify-center overflow-hidden text-lg text-white"
             title="loading"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="64"
-              height="64"
-              fill="currentColor"
-              className="bi bi-arrow-clockwise"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"
-              />
-              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
-            </svg>
+            <div className="animate-spin">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="64"
+                height="64"
+                fill="currentColor"
+                className="bi bi-arrow-clockwise"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"
+                />
+                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+              </svg>
+            </div>
           </div>
         )}
         {searchDataStatus === "error" && (
