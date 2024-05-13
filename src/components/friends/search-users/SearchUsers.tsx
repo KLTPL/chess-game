0b6a5750e0371449b61c7bffc945e276ccf2e993
Kacper Lebiedzinski <kltpl@duck.com>
2024-05-13 15:49:45 +0200

    added icons instead of text messages when search result on /friend route is loading or when there is an error

--- src/components/friends/search-users/SearchUsers.tsx
+++ src/components/friends/search-users/SearchUsers.tsx
@@ -101,13 +101,42 @@ export default function SearchUsers({
       />
       <div className="my-3 flex w-[97vw] grow flex-col items-stretch self-center overflow-auto md:w-[70ch]">
         {searchDataStatus === "loading" && (
-          <div className="flex grow items-center justify-center text-lg text-white">
-            Ładowanie
+          <div
+            className="mt-3 flex grow animate-spin items-center justify-center text-lg text-white"
+            title="loading"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="64"
+              height="64"
+              fill="currentColor"
+              className="bi bi-arrow-clockwise"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"
+              />
+              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+            </svg>
           </div>
         )}
         {searchDataStatus === "error" && (
-          <div className="flex grow items-center justify-center text-lg text-red-700">
-            Bład
+          <div
+            className="mt-3 flex grow items-center justify-center text-lg text-red-700"
+            title="error"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="64"
+              height="64"
+              fill="currentColor"
+              className="bi bi-x-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+            </svg>
           </div>
         )}
         {searchDataStatus === "success" &&
