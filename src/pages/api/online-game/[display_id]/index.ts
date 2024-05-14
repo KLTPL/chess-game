be0caf36bb9f17ad6d6d92f094f56c1339478acc
Kacper Lebiedziński <k.lebiedzinski04.gmail.com>
2024-05-14 18:01:46 +0200

    Deleted api enpoints which where not used from the /api/online-game route

--- src/pages/api/online-game/[display_id]/index.ts
+++ src/pages/api/online-game/[display_id]/index.ts
@@ -1,44 +1,11 @@
 import { type APIRoute } from "astro";
-import getGameData from "../../../../db/game/getGameData";
 import addNewMove from "../../../../db/game-halfmove/addNewMove";
 import updateGameResult from "../../../../db/game/updateGameResult";
-import type {
-  APIGetOnlineGame,
-  APIGetPostHalfmove,
-} from "../../../../db/types";
+import type { APIGetPostHalfmove } from "../../../../db/types";
 import isUserAllowedToMove from "../../../../db/game/isUserAllowedToMove";
 import isMoveValid from "../../../../db/game/isMoveValid";
 import { getGameId } from "../../../../db/game/convertGameIds";
 import OnlineGameController from "../../../../scripts-server/onlineGameController";
-
-export const GET: APIRoute<APIGetOnlineGame> = async ({ params, locals }) => {
-  try {
-    const data = await getGameData(params.display_id as string);
-
-    if (data === null) {
-      return new Response(null, {
-        status: 404,
-        statusText: "Not found",
-      });
-    }
-
-    return new Response(
-      JSON.stringify({ getDBGameData: data, userId: locals.user?.id }),
-      {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(error.message);
-      return new Response(null, { status: 500, statusText: error.message });
-    }
-    return new Response(null, { status: 500 });
-  }
-};
 
 export const POST: APIRoute = async ({ request, locals, url, params }) => {
   try {
@@ -94,33 +61,3 @@ export const POST: APIRoute = async ({ request, locals, url, params }) => {
     return new Response(null, { status: 500 });
   }
 };
-
-// export const PUT: APIRoute = async ({ request, locals, url }) => {
-//   try {
-//     const data: PutDBGame = await request.json();
-//     const { user } = locals;
-//     if (user === undefined) {
-//       throw new Error(
-//         `User (locals.user) is not defined in a protected route ${url.pathname}`
-//       );
-//     }
-//     const isAllowedOrStatusCode = await isUserAllowedToMove(user, data.id);
-//     if (isAllowedOrStatusCode === true) {
-//       updateGameResult(data);
-//       return new Response(null, {
-//         status: 200,
-//       });
-//     }
-
-//     return new Response(null, {
-//       status: isAllowedOrStatusCode.code,
-//       statusText: isAllowedOrStatusCode.message,
-//     });
-//   } catch (error) {
-//     if (error instanceof Error) {
-//       console.error(error.message);
-//       return new Response(null, { status: 500, statusText: error.message });
-//     }
-//     return new Response(null, { status: 500 });
-//   }
-// };
